a5d8106383be9f3878892201ab1c563548afac04
lubby <514627495@qq.com>
2015-09-29 15:42:27 +0800

    construct builder

---
+++ srv-business/src/main/java/com/lubby/service/BusinessService.java
@@ -0,0 +1,7 @@
+package com.lubby.service;
+
+/**
+ * Created by liubin on 2015-9-29.
+ */
+public interface BusinessService {
+}
---
+++ srv-business/src/main/java/com/lubby/service/impl/BusinessServiceImpl.java
@@ -0,0 +1,7 @@
+package com.lubby.service.impl;
+
+/**
+ * Created by liubin on 2015-9-29.
+ */
+public class BusinessServiceImpl {
+}
