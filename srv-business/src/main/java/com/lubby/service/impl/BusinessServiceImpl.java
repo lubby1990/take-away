49ce9fc1c9dc3b39f682fa7d41d9bab0b364f067
lubby <514627495@qq.com>
2015-09-30 18:36:36 +0800

    项目骨架设计

--- srv-business/src/main/java/com/lubby/service/impl/BusinessServiceImpl.java
+++ srv-business/src/main/java/com/lubby/service/impl/BusinessServiceImpl.java
@@ -1,7 +1,13 @@
 package com.lubby.service.impl;
 
+import org.springframework.cache.annotation.SpringCacheAnnotationParser;
+
 /**
  * Created by liubin on 2015-9-29.
  */
 public class BusinessServiceImpl {
+    public static void main(String[] ss) {
+
+
+    }
 }
